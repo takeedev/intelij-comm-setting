e6a8a5577d0de04e2163030eb9ea1233607fd324
taknaja <peerapolkoeram@gmail.com>
2024-12-03 23:56:42 +0700

    delete c

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@
 config_path_last_version=$(ls -d $HOME/AppData/Roaming/JetBrains/IdeaIC* | sort -V | tail -n 1)
 echo "Latest version: $config_path_last_version"
 
+
 curl -k https://raw.githubusercontent.com/takeedev/setting-intelij-comm/refs/heads/main/config/options/editor.xml -o "$config_path_last_version/options/editor_Test_Git.xml"
